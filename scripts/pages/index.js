102ba6b0b55708e952683af15c10fe034e916ff3
Stéphanie Coupama <frimouss.graphweb@gmail.com>
2025-06-20 12:03:26 +0400

    test: ajout de console.log pour repérage des fonctions

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,55 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+async function getPhotographers() {
+  // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
+  // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+  //console.log("[getPhotographers] Fonction appelée");
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+  let photographers = [
+    {
+      name: "Ma data test",
+      id: 1,
+      city: "Paris",
+      country: "France",
+      tagline: "Ceci est ma data test",
+      price: 400,
+      portrait: "account.png",
+    },
+    {
+      name: "Autre data test",
+      id: 2,
+      city: "Londres",
+      country: "UK",
+      tagline: "Ceci est ma data test 2",
+      price: 500,
+      portrait: "account.png",
+    },
+  ];
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+  //console.log("[getPhotographers] Données récupérées :", photographers);
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    }
-    
-    init();
-    
+  // et bien retourner le tableau photographers seulement une fois récupéré
+  return {
+    photographers: [...photographers, ...photographers, ...photographers],
+  };
+}
+
+async function displayData(photographers) {
+  //console.log("[displayData] Données reçues :", photographers);
+  const photographersSection = document.querySelector(".photographer_section");
+
+  photographers.forEach((photographer) => {
+    //console.log("[displayData] Photographe en cours :", photographer.name);
+    const photographerModel = photographerTemplate(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+async function init() {
+  //console.log("[init] Initialisation");
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  //console.log("[init] Données retournées par getPhotographers :", photographers);
+  displayData(photographers);
+}
+
+init();
