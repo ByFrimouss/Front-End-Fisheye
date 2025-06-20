1012dcc43c8b74e565e005ee8aae0a3330d90ce1
Stéphanie Coupama <frimouss.graphweb@gmail.com>
2025-06-20 15:52:18 +0400

    oublie de sauvegarder apres commentaires

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -2,6 +2,7 @@ async function getPhotographers() {
   // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
   // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
 
+  // Récupération des ressources JSON via l'API fetch()
   //  ⇩⇩⇩⇩⇩
   try {
     const response = await fetch("./data/photographers.json");
